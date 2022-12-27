fa352bba5fe7a5f063705a6bfed9bf18f5812a0f
krapivnitskiy <makartut374@gmail.com>
2022-12-27 11:09:54 +0300

    fixed cucumber launch

--- src/main/java/base/CukesFeatureRunner.java
+++ src/main/java/base/CukesFeatureRunner.java
@@ -1,14 +1,10 @@
 package base;
 
-
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.junit.Cucumber;
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.cucumber.junit.CucumberOptions;
@@ -19,9 +15,18 @@ import static base.ChromeDriveOptions.getOptions;
 @CucumberOptions(
         features = {"src/main/resources/Authorization.feature"},
         glue = {"steps"}
-
 )
 public class CukesFeatureRunner {
 
+    public static WebDriver driver;
 
+    @Before
+    public void beforeClass() {
+        driver = new ChromeDriver(getOptions());
+    }
+
+    @After
+    public void afterClass() {
+        driver.quit();
+    }
 }
